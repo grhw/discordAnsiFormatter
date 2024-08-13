e3c268ec4b4f561086cc7730bd9dfa680f51b54a
Gust Who? <lucgustga@outlook.com>
2024-08-13 18:40:45 +0800

    format

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,15 @@
 local convert = (function()
     local magic_symbol = "["
 
-    function split(inputstr, sep) sep=sep or '%s' local t={}  for field,s in string.gmatch(inputstr, "([^"..sep.."]*)("..sep.."?)") do table.insert(t,field)  if s=="" then return t end end end
+    function split(inputstr, sep)
+        sep = sep or '%s'
+        local t = {}
+        for field, s in string.gmatch(inputstr,
+                                      "([^" .. sep .. "]*)(" .. sep .. "?)") do
+            table.insert(t, field)
+            if s == "" then return t end
+        end
+    end
 
     local codes = {
         ["color"] = {
@@ -12,20 +20,16 @@ local convert = (function()
             ["blue"] = "34",
             ["pink"] = "35",
             ["aqua"] = "36",
-            ["white"] = "37",
+            ["white"] = "37"
         },
         ["background"] = {
             ["dark"] = "40",
             ["clear"] = "0",
             ["light"] = "47",
             ["highlight"] = "45",
-            ["warn"] = "41",
+            ["warn"] = "41"
         },
-        ["format"] = {
-            ["reset"] = "0",
-            ["bold"] = "1",
-            ["under"] = "4",
-        }
+        ["format"] = {["reset"] = "0", ["bold"] = "1", ["under"] = "4"}
     }
 
     local function guess_code_type(code)
@@ -41,36 +45,33 @@ local convert = (function()
     local function capture_commands(str)
         local final = {}
         local f = 0
-        for a, b, c in string.gmatch(str, "&([a-zA-Z]+)%-([a-zA-Z]+)%-([a-zA-Z]+);") do
+        for a, b, c in string.gmatch(str,
+                                     "&([a-zA-Z]+)%-([a-zA-Z]+)%-([a-zA-Z]+);") do
             local e = "&" .. a .. "-" .. b .. "-" .. c .. ";"
-            local d = str:find(e,f+1,true)
+            local d = str:find(e, f + 1, true)
             f = d
-            table.insert(final,{d,e})
+            table.insert(final, {d, e})
         end
         f = 0
         for a, b in string.gmatch(str, "&([a-zA-Z]+)%-([a-zA-Z]+);") do
             local e = "&" .. a .. "-" .. b .. ";"
-            local d = str:find(e,f+1,true)
+            local d = str:find(e, f + 1, true)
             f = d
-            table.insert(final,{d,e})
+            table.insert(final, {d, e})
         end
         f = 0
         for a in string.gmatch(str, "&([a-zA-Z]+);") do
             local e = "&" .. a .. ";"
-            local d = str:find(e,f+1,true)
+            local d = str:find(e, f + 1, true)
             f = d
-            table.insert(final,{d,e})
+            table.insert(final, {d, e})
         end
 
-        table.sort(final,function(a,b)
-            return a[1] < b[1]
-        end)
+        table.sort(final, function(a, b) return a[1] < b[1] end)
 
         local matches = {}
 
-        for _, v in pairs(final) do
-            table.insert(matches, v[2])
-        end
+        for _, v in pairs(final) do table.insert(matches, v[2]) end
 
         return matches
     end
@@ -84,15 +85,18 @@ local convert = (function()
             ["color"] = codes["color"]["white"]
         }
 
-        for _,command in pairs(capture_commands(str)) do
-            for _,code in pairs(split(command:sub(2,command:len()-1),"%-")) do
+        for _, command in pairs(capture_commands(str)) do
+            for _, code in pairs(split(command:sub(2, command:len() - 1), "%-")) do
                 local code_type = guess_code_type(code)
-                assert(code_type,"'"..code.."' is an invalid command.\n\n")
+                assert(code_type, "'" .. code .. "' is an invalid command.\n\n")
                 current[code_type] = codes[code_type][code]
             end
-            
-            local cmd = current["format"]..";"..current["background"]..";"..current["color"].."m"
-            final = string.gsub(final,command:gsub("%-","%%%-"),magic_symbol..cmd,1)
+
+            local cmd =
+                current["format"] .. ";" .. current["background"] .. ";" ..
+                    current["color"] .. "m"
+            final = string.gsub(final, command:gsub("%-", "%%%-"),
+                                magic_symbol .. cmd, 1)
         end
         return final
     end
@@ -123,16 +127,17 @@ end)()
 local io = require("io")
 
 if args[1] == "file" or args[1] == "preview" then
-    local fn = table.concat(args," ",2)
-    local file = io.open(fn,"r")
+    local fn = table.concat(args, " ", 2)
+    local file = io.open(fn, "r")
     if file then
         local content = file:read("a")
         file:close()
 
         local converted = convert(content)
         if args[1] == "file" then
-            local writing = io.open(fn.."_conv.dsansi","w+")
-            assert(writing,"that's not supposed to happen! check if you have permissions.")
+            local writing = io.open(fn .. "_conv.dsansi", "w+")
+            assert(writing,
+                   "that's not supposed to happen! check if you have permissions.")
             writing:write(converted)
             writing:close()
         else
@@ -142,7 +147,7 @@ if args[1] == "file" or args[1] == "preview" then
         print(convert(messages.no_file))
     end
 elseif args[1] == "text" then
-    print(convert(table.concat(args," ",2)))
+    print(convert(table.concat(args, " ", 2)))
 else
     print(convert(messages.help_command)) -- ds_af help doesn't actually exist
 end
